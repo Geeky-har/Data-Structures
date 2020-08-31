4093ae978fa0c0abf7ae5824ff86b881ce16d4a0
Harsh negi <harshnegi6477@gmail.com>
2020-09-01 00:26:25 +0530

    added comments and int to bool in check functions

--- Stack/stack_implementation.cpp
+++ Stack/stack_implementation.cpp
@@ -1,63 +1,65 @@
+// In the following program I have implemented Stack ADT using Arrays
+
 #include<bits/stdc++.h>
 using namespace std;
 
-class ArrayStack{
+class ArrayStack{	// class for array stack
 	int top;
 	int size;
 	int *array;
 
 	public:
 		ArrayStack* CreateStack();
-		int isFull(ArrayStack*);
-		int isEmpty(ArrayStack*);
-		void push(ArrayStack*);
+		bool isFull(ArrayStack*);		// Declaration of all 
+		bool isEmpty(ArrayStack*);		//		the function which
+		void push(ArrayStack*);			//			are used in the program
 		void pop(ArrayStack*);
 		void printStack(ArrayStack*);
 
 };
 
 
-ArrayStack* ArrayStack:: CreateStack(){
-	struct ArrayStack *stack = new struct ArrayStack;
+ArrayStack* ArrayStack:: CreateStack(){		// function for creating a stack of defined size
+	ArrayStack *stack = new ArrayStack;
 	int n;
 	cout<<"Please Enter the size of the stack: "<<endl;
 	cin>>n;
 	
-	stack->top = -1;
-	stack->size = n;
-	stack->array = new int[n];
+	stack->top = -1;	// Initializing top as -1(no elements)
+	stack->size = n;	// Initializing the size of stack
+	stack->array = new int[n];	// creating the array dynamically
 	
 	cout<<"Your Stack with size "<<n<<" created"<<endl<<endl;
 	
-	return(stack);
+	return(stack);		// returned the object with above specifications
 }
 
-int ArrayStack:: isFull(ArrayStack *stack){
+bool ArrayStack:: isFull(ArrayStack *stack){		// method to check if array is full?
 	
 	if(stack->top == stack->size-1)
-		return(1);
+		return(true);		// return true if full
 		
 	else
-		return(0);
+		return(false);		// return false if not full
 }
 
-int ArrayStack:: isEmpty(ArrayStack *stack){
+bool ArrayStack:: isEmpty(ArrayStack *stack){	// method to check if array is empty?
 	
 	if(stack->top == -1)
-		return(1);
+		return(true);		// returns true if empty
 		
-	return(0);
+	return(false);			// returns false if not empty
 }
 
-void ArrayStack:: push(ArrayStack *stack){
+void ArrayStack:: push(ArrayStack *stack){		// method for push operation
 	int item;
 	
-	if(!isFull(stack)){
+	if(!isFull(stack)){	// execute if stack is not full
 		cout<<"Enter the element that they wanna push into the stack: "<<endl;
-		cin>>item;
+		cin>>item;		// item stores the entered element
 		
-		stack->top = stack->top + 1;
-		stack->array[stack->top] = item;
+		stack->top = stack->top + 1;	// top value increemented
+		stack->array[stack->top] = item;		// item stored in the array with index top
 		cout<<"Item pushed successfully!!"<<endl;
 	}
 	
@@ -65,12 +67,12 @@ void ArrayStack:: push(ArrayStack *stack){
 		cout<<"The stack is full (overflow condition!!)"<<endl;
 }
 
-void ArrayStack:: pop(ArrayStack *stack){
+void ArrayStack:: pop(ArrayStack *stack){		// method for pop operation
 	int temp;
 	
-	if(!isEmpty(stack)){
-		temp = stack->array[stack->top];
-		stack->top--;
+	if(!isEmpty(stack)){	// execute if stack not empty
+		temp = stack->array[stack->top];	// temp will store the value of topmost element
+		stack->top--;		// value of top decreemented
 		cout<<"The element which is deleted is: "<<temp<<endl;
 	}
 	
@@ -79,13 +81,13 @@ void ArrayStack:: pop(ArrayStack *stack){
 	
 }
 
-void  ArrayStack:: printStack(ArrayStack *stack){
+void  ArrayStack:: printStack(ArrayStack *stack){	// for printing the elements in the stack
 	int i;
 	if(isEmpty(stack)){
 		cout<<"The stack is empty, please perform push operation"<<endl;
 	}
 	
-	else{
+	else{	// will execute if the stack is not empty
 		cout<<"The elements inside the stack are: ";
 		
 		for(i=0; i<=stack->top; i++){
@@ -98,7 +100,7 @@ void  ArrayStack:: printStack(ArrayStack *stack){
 int main(){
 	int option;
 	ArrayStack obj;
-	ArrayStack *stack;
+	ArrayStack *stack;	// pointer object of ArrayStack type
 	cout<<"Please select an option from below: "<<endl<<endl;
 	
 	do{
@@ -111,16 +113,16 @@ int main(){
 		cin>>option;
 		
 		switch(option){
-			case 1: stack = obj.CreateStack();
+			case 1: stack = obj.CreateStack();		// pointer stores the address of new class
 					break;
 						
-			case 2: obj.push(stack);
+			case 2: obj.push(stack);		// push operation with class pointer as args
 					break;
 						
-			case 3: obj.pop(stack);
+			case 3: obj.pop(stack);			// pop operation with class pointer as args
 					break;
 						
-			case 4: obj.printStack(stack);
+			case 4: obj.printStack(stack);  // print operation with class pointer as args
 					break;
 		}
 			
