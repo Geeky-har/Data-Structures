313013dd5ac4b69a6b493d2c79deec7ac697530c
Harsh negi <harshnegi6477@gmail.com>
2020-09-18 00:06:28 +0530

    added delete from anywhere functionality

--- Circular Linked List/CLL.cpp
+++ Circular Linked List/CLL.cpp
@@ -21,6 +21,7 @@ public:
     void printList();
     void insertBeg();
     void deleteBeg();
+    void deleteAny();
     void insertList();
 };
 
@@ -83,7 +84,7 @@ void CLLNode::printList()
         }
 
         if (t->next == start)
-            cout << t->data<<endl;
+            cout << t->data << endl;
     }
 }
 
@@ -145,48 +146,128 @@ void CLLNode::deleteBeg()
     }
 }
 
-void CLLNode::insertList(){
-    CLLNode* t = start;
-    CLLNode* newNode;
+void CLLNode::insertList()
+{
+    CLLNode *t = start;
+    CLLNode *newNode;
 
-    if(t == nullptr)
-        cout<<"The list is empty!!"<<endl;
+    if (t == nullptr)
+        cout << "The list is empty!!" << endl;
 
-    else{
+    else
+    {
         int data, key;
 
-        cout<<"Enter the element after which you want to perform insertion: "<<endl;
-        cin>>key;
+        cout << "Enter the element after which you want to perform insertion: " << endl;
+        cin >> key;
 
-        cout<<"Now enter the value of new element: "<<endl;
-        cin>>data;
+        cout << "Now enter the value of new element: " << endl;
+        cin >> data;
 
-        while(t->next != start && t->data != key)
+        while (t->next != start && t->data != key)
             t = t->next;
 
-        if(t->next == start && t->data != key)
-            cout<<"The key you entered is not present in the list"<<endl;
+        if (t->next == start && t->data != key)
+            cout << "The key you entered is not present in the list" << endl;
 
-        else if(t->next == start && t->data == key){
+        else if (t->next == start && t->data == key)
+        {
             newNode = createNode();
             newNode->data = data;
 
             t->next = newNode;
             newNode->next = start;
 
-            cout<<"New node added successfully!!"<<endl;
+            cout << "New node added successfully!!" << endl;
         }
 
-        else{
-            CLLNode* temp = t->next;
+        else
+        {
+            CLLNode *temp = t->next;
             newNode = createNode();
             newNode->data = data;
             t->next = newNode;
             newNode->next = temp;
 
-            cout<<"New node added successfully!!"<<endl;
+            cout << "New node added successfully!!" << endl;
+        }
+    }
+}
+
+void CLLNode::deleteAny()
+{
+    CLLNode *t = start;
+    CLLNode *t1 = nullptr;
+
+    if (t == nullptr)
+        cout << "The list is empty!" << endl;
+
+    else
+    {
+        int del_data;
+        cout << "Enter the element that you want to delete from the list: " << endl;
+        cin >> del_data;
+
+        if (t->data == del_data)
+        {
+            int data;
+            CLLNode *temp = start;
+            data = t->data;
+
+            if (t->next == start)
+            {
+                start = nullptr;
+                delete(t);
+            }
+
+            else
+            {
+                while (t->next != start)
+                    t = t->next;
+
+                t->next = start->next;
+                start = start->next;
+                delete(temp);
+            }
+
+            cout<<"The element "<<data<<" is deleted from the list"<<endl;
         }
 
+        else
+        {
+
+            while (t->next != start && t->data != del_data)
+            {
+                t1 = t;
+                t = t->next;
+            }
+
+            if (t->next == start && t->data != del_data)
+            {
+                cout << "The key you entered is not present in the list" << endl;
+            }
+
+            else if (t->next == start && t->data == del_data)
+            {
+                int del;
+                t1->next = start;
+                del = t->data;
+                delete (t);
+
+                cout << "The element which is deleted is: " << del << endl;
+            }
+
+            else
+            {
+                int del;
+                CLLNode *temp = t->next;
+                t1->next = temp;
+                del = t->data;
+                delete (t);
+
+                cout << "The element which is deleted is: " << del << endl;
+            }
+        }
     }
 }
 
@@ -206,7 +287,8 @@ int main()
         cout << "3. To insert an element in the beg" << endl;
         cout << "4. To insert an element anywhere in the list" << endl;
         cout << "5. To delete an element from the beg" << endl;
-        cout << "6. Exit" << endl;
+        cout << "6. To delete an element from anywhere" << endl;
+        cout << "7. Exit" << endl;
         cin >> option;
 
         switch (option)
@@ -230,9 +312,13 @@ int main()
         case 5:
             obj.deleteBeg();
             break;
+
+        case 6:
+            obj.deleteAny();
+            break;
         }
 
-    } while (option != 6);
+    } while (option != 7);
 
     return 0;
 }
